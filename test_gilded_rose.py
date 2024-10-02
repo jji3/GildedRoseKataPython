73eb4f009b52919543a8d89962a241682993a6c8
Jie Ji <inkpark@hotmail.com>
2024-10-01 20:21:00 -0700

    lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,32 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+    def test_Aged_Brie_should_increase(self):
+        ab = "Aged Brie"
+        items = [Item(ab, 3, 2)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEquals(1, items[0].quality)
+
+    def test_Sulfuras_should_not_change(self):
+        sul = "Sulfuras, Hand of Ragnaros"
+        items = [Item(sul, 5, 50)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEquals(45, items[0].quality)
+
+    def test_quality_never_negative(self):
+        cake = "Conjured Mana Cake"
+        items = [Item(cake, 5, 0)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEquals(-1, items[0].quality)
+
+    # def test_foo(self):
+    #     items = [Item("foo", 0, 0)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     self.assertEquals("fixme", items[0].name)
 
 
 if __name__ == '__main__':
